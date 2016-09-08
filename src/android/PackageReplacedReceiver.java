a13e4518fa7cf03cab4830886f170058b3c15287
James Robinson <jameskennethrobinson@gmail.com>
2016-09-08 14:50:29 -0700

    Update PackageReplacedReceiver.java

--- src/android/PackageReplacedReceiver.java
+++ src/android/PackageReplacedReceiver.java
@@ -1,16 +1,29 @@
+
 package com.tonikorin.cordova.plugin.autostart;
+
+import com.transistorsoft.cordova.bggeo.BootReceiver;
+
+import android.os.Bundle;
+import android.util.Log;
+import org.json.JSONArray;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import com.tonikorin.cordova.plugin.autostart.AppStarter;
+import android.util.Log;
+
  
 public class PackageReplacedReceiver extends BroadcastReceiver {
     
     @Override
     public void onReceive(Context context, Intent intent) {
-    
-        AppStarter appStarter = new AppStarter();
-        appStarter.run(context, intent, AppStarter.BYPASS_USERPRESENT_MODIFICATION );
+        Log.d("Truckerline", "Package Replaced!!! yba");
+
+        BootReceiver br = new BootReceiver();
+
+        br.onReceive(context, intent);
+        
+        // AppStarter appStarter = new AppStarter();
+        // appStarter.run(context, intent, AppStarter.BYPASS_USERPRESENT_MODIFICATION );
     }
 }
