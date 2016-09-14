ee94206826ffe60ce87c0d31b0db365bfcde7e05
James Robinson <james@truckerline.com>
2016-09-14 13:04:34 -0700

    Add boot and package replaced receiver

--- src/android/PackageReplacedReceiverZenDrive.java
+++ src/android/PackageReplacedReceiverZenDrive.java
@@ -1,24 +1,29 @@
+
 package com.tonikorin.cordova.plugin.autostart;
+
+import com.transistorsoft.cordova.bggeo.BootReceiver;
+
+import android.os.Bundle;
+import android.util.Log;
+import org.json.JSONArray;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import com.tonikorin.cordova.plugin.autostart.AppStarter;
 import android.util.Log;
 
-
 import com.zendrive.sdk.receiver.WakeupReceiver
+
  
-public class BootCompletedReceiver extends BroadcastReceiver {
+public class PackageReplacedReceiver extends BroadcastReceiver {
     
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("Truckerline", "Wakeup Occured!!! yba");
-        
+        Log.d("Truckerline", "ZenDrive Package Replaced!!! yba");
+
         WakeupReceiver wr = new WakeupReceiver();
 
-        wr.onReceive(context, intent);
-        // appStarter.run(context, intent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED);
+        wr.onReceive(context, intent)
+
     }
 }
